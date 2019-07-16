41002608419b4338619a93f2f3d6485983b572ae
CSÉCSY László <laszlo.csecsy@pronovix.com>
2019-07-16 09:53:09 +0200

    Fix `MinimumLength.php`
    closes #34

--- src/Plugin/PasswordConstraint/MinimumLength.php
+++ src/Plugin/PasswordConstraint/MinimumLength.php
@@ -3,12 +3,12 @@
 namespace Drupal\password_enhancements\Plugin\PasswordConstraint;
 
 /**
- * Upper-case password constraint plugin.
+ * Minimum length password constraint plugin.
  *
  * @PasswordConstraint(
  *   id = "minimum_length",
  *   name = @Translation("Minimum length"),
- *   description = @Translation("Checks if the password has at least a specified number of character of any type."),
+ *   description = @Translation("Checks if the password has at least a specified number of characters of any type."),
  *   unique = TRUE,
  *   jsLibrary = "password_enhancements/plugin.minimum_length",
  * )
@@ -19,14 +19,14 @@ final class MinimumLength extends MinimumCharacters {
    * {@inheritdoc}
    */
   public function defaultDescriptionSingular(): string {
-    return 'Add at least one letter.';
+    return 'Add at least one character.';
   }
 
   /**
    * {@inheritdoc}
    */
   public function defaultDescriptionPlural(): string {
-    return 'Add @minimum_characters more letters.';
+    return 'Add @minimum_characters more characters.';
   }
 
 }
